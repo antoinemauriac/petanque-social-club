9dbd2a3a1fb6972282d9b9feecb3b57a576d79a3
Antoine Mauriac <mauriac.antoine@gmail.com>
2022-11-29 12:06:21 +0100

    rien

--- app/models/game_team.rb
+++ app/models/game_team.rb
@@ -1,4 +1,8 @@
 class GameTeam < ApplicationRecord
   belongs_to :game
   belongs_to :team
+
+  def self.teams_of_the_game(game)
+    where(game: game).map { |game| game.team }
+  end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,9 @@
 class Team < ApplicationRecord
   has_many :game_teams
   has_many :games, through: :game_teams
+
   belongs_to :league
+
   has_many :teams_users
   has_many :users, through: :team_users
 end
