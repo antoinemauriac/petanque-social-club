14bbac1cb0931dfd22c3b1d6b8ee43fadd0ad51e
Antoine Mauriac <mauriac.antoine@gmail.com>
2022-11-29 18:16:26 +0100

    score amélioré

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   resources :leagues, only: %i[new create show index] do
-    resources :games, only: %i[new create show edit update index]
+    resources :games, only: %i[new create show edit index]
   end
+  resources :games, only: :update
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
