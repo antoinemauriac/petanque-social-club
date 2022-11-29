a7919c33bd682c2c461abf97a8b7732932aac94e
lucas fabre <lucasfabre86@proton.me>
2022-11-29 18:06:44 +0100

    stimulus click a corriger, links

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import DisplayController from "./display_controller"
+application.register("display", DisplayController)
