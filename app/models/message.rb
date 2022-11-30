f2cd0edfc0a014e3a376a182e5f84bf8da74efb6
lucas fabre <lucasfabre86@proton.me>
2022-11-30 16:30:04 +0100

    chat quasi fini

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,6 @@ class Message < ApplicationRecord
   belongs_to :league
   belongs_to :user
 
-  validates :comment, length: { in: 1..100 }
+  # validates :comment, length: { in: 1..100 }
+  validates :comment, length: { minimum: 1 }
 end
