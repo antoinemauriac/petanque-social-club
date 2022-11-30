5e1eab0c99ac81d321b21f91dad8dadfb6641121
lucas fabre <lucasfabre86@proton.me>
2022-11-30 15:10:30 +0100

    typedjs

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ application.register("hello", HelloController)
 
 import DisplayController from "./display_controller"
 application.register("display", DisplayController)
+
+import TypedJsController from "./typed_js_controller"
+application.register("typed-js", TypedJsController)
