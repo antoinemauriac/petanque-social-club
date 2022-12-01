1009775f2294da0cc061b20355a6da6499fafdec
intissar chakour <chakourintissar08@hotmail.com>
2022-12-01 14:12:57 +0100

    display field/map

---
+++ app/javascript/controllers/terrain_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["fields", "cartes", "field", "carte"]
+
+  connect(event) {
+    event.preventDefault
+  }
+
+  showfields(event) {
+    event.preventDefault();
+    this.fieldTarget.classList.add("active")
+    this.carteTarget.classList.remove("active")
+    this.fieldsTarget.classList.remove("d-none")
+    this.cartesTarget.classList.add("d-none")
+  }
+
+  showcartes(event) {
+    event.preventDefault();
+    this.fieldTarget.classList.remove("active")
+    this.carteTarget.classList.add("active")
+    this.fieldsTarget.classList.add("d-none")
+    this.cartesTarget.classList.remove("d-none")
+  }
+
+}
